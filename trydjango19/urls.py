fa179c018826337564f214b64c7f8b6fcfd4378f
Evan B <admin@storm-east.com>
2018-06-11 03:52:51 -0600

    28_1-File uploads with FileField

--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from posts import views
 
 urlpatterns = [
@@ -28,3 +30,7 @@ urlpatterns = [
     url(r'^posts/(?P<id>\d+)/delete/$', views.post_delete, name="delete"),
     url(r'^posts/create/$', views.post_create, name="create"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
